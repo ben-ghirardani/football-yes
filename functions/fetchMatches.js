b8afad834e3850ea007957a1764e3c8eb0bf80c8
Ben <pretty.fiendish@gmail.com>
2020-04-21 23:02:46 +0100

    resolving clash with http and https

--- functions/fetchMatches.js
+++ functions/fetchMatches.js
@@ -4,11 +4,7 @@ const { AUTH_TOKEN } = process.env;
 
 exports.handler = function(event, context, callback) {
 
-
-  // header for CORS?
-
   return fetch(`https://api.football-data.org/v2/competitions/2021/matches`, 
-    // access-control-allow-origin comes in request in App.js not here?
     {headers : {'X-Auth-Token': AUTH_TOKEN, 'Access-Control-Allow-Origin': '*' } } )
       .then(response => response.json())
       .then(response => {
