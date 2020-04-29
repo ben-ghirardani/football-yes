b1f4c7b68e36a4226e2fcdca1677f5e401dd4998
Ben <pretty.fiendish@gmail.com>
2020-04-29 13:32:33 +0100

    updated api endpoint to a more recent version

--- functions/fetchMatches.js
+++ functions/fetchMatches.js
@@ -4,7 +4,8 @@ const { AUTH_TOKEN } = process.env;
 
 exports.handler = function(event, context, callback) {
 
-  return fetch(`https://api.football-data.org/v2/competitions/2021/matches`, 
+  // return fetch(`https://api.football-data.org/v2/competitions/2021/matches`,
+  return fetch(`https://api.football-data.org/v2/competitions/PL/matches`, 
     {headers : {'X-Auth-Token': AUTH_TOKEN, 'Access-Control-Allow-Origin': '*' } } )
       .then(response => response.json())
       .then(response => {
